87dd25bed00aec5978b8218d516ddfa71532fdab
diemscse <sachinkadam313@gmail.com>
2022-01-16 23:32:43 +0530

    remove files
    s

--- FRB/__init__.py
+++ FRB/__init__.py
@@ -3,17 +3,16 @@ from __future__ import absolute_import
 try:
     from . import search_FRB
     from .search_FRB import search_FRB
-    from .guppi import GuppiRaw
     from . import utils
-    from . import fil2h5
-    from . import h52fil
-    from . import h5diag
-    from . import bl_scrunch
-    from . import calcload
-    from . import rawhdr
-    from . import stax
-    from . import stix
-    from . import match_fils
+    # from . import fil2h5
+    # from . import h52fil
+    # from . import h5diag
+    # from . import bl_scrunch
+    # from . import calcload
+    # from . import rawhdr
+    # from . import stax
+    # from . import stix
+    # from . import match_fils
     from FRB.io import file_wrapper
 except:
     print("Warning: At least one utility could not be imported!")
